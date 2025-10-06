119f4ad2e8ea1ec119825981f2cc72dcf6c93c4e
Scott <96879441+scottfridwin@users.noreply.github.com>
2025-10-06 01:12:34 +0000

    Another script fix.

--- lidarr/scripts_init.bash
+++ lidarr/scripts_init.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bash
 set -euo pipefail
 
-export GITHUB_URL="https://raw.githubusercontent.com/scottfridwin/arr-scripts/main"
+GITHUB_URL="https://raw.githubusercontent.com/scottfridwin/arr-scripts/main"
 curl -sfL $GITHUB_URL/lidarr/setup.bash | bash
 exit
